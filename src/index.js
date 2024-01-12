15713081d69104e80e0ad3391620ebd3af34aaae
bhavik doshi <82199211+bhavik25-cpu@users.noreply.github.com>
2024-01-12 19:47:49 +0530

    changesdone

--- src/index.js
+++ src/index.js
@@ -1,27 +1,3 @@
-// const express = require('express');
-// const dotenv = require('dotenv');
-// const usersRoutes = require('./routes/usersRoutes');
-
-// dotenv.config();
-
-// const app = express();
-// const PORT = process.env.PORT || 3000;
-
-// app.use(express.json());
-// app.use('/api', usersRoutes);
-
-// app.use((req, res) => {
-//   res.status(404).json({ error: 'Route not found' });
-// });
-
-// app.use((err, req, res, next) => {
-//   console.error(err.stack);
-//   res.status(500).json({ error: 'Internal Server Error' });
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
 const express = require('express');
 const dotenv = require('dotenv');
 const usersRoutes = require('./routes/usersRoutes');
